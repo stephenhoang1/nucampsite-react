6ef239407aaf1d233e940282ffcacc5a3b0dede7
stephenhoang1 <s.hoang17@gmail.com>
2020-07-17 19:18:13 -0700

    Intro to Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -80,9 +80,7 @@ class Header extends Component {
                                 </NavItem>
                             </Nav>
                             <span className="navbar-text ml-auto">
-                                <Button outline onClick={this.toggleModal}>
-                                    <i className="fa fa-sign-in fa-lg" /> Login
-                                </Button>
+                                learn
                             </span>
                         </Collapse>
                     </div>
